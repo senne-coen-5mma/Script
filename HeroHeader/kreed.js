8cbd25c06456902aa65d52eb4eee99a6c3e6b9e9
senne-coen-5mma <cs061121@tsmmechelen.net>
2023-11-09 09:39:29 +0100

    uh?

--- HeroHeader/kreed.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    const navLinks = document.getElementById("navLinks");
-    if (navLinks.style.display === "block") {
-        navLinks.style.display = "none";
-    } else {
-        navLinks.style.display = "block";
-    }
-}
